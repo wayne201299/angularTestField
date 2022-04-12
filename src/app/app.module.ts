97a7fb9b8d6daffad1e9d06a5d2d9073172e5552
Alan Zhuang <wayne20129954@gmail.com>
2022-04-12 10:34:50 +0800

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const declarations = [AppComponent, NavbarComponent];
+const imports = [BrowserModule, AppRoutingModule, BrowserAnimationsModule];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: declarations,
+  imports: imports,
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
